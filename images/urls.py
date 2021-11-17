7c8bb0b0ab34f3cf6c7479aa2b9be5a1ad2c9bff
KwabenaYeboah <kwabena4140@gmail.com>
2021-11-17 16:12:15 +0000

    Added AJAX functionality to like and unlike images

--- images/urls.py
+++ images/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import upload_image_view, image_detail_view
+from .views import upload_image_view, image_detail_view, like_image_view
 
 urlpatterns = [
     path('upload/', upload_image_view, name='upload'),
     path('detail/<int:id>/<slug:slug>/', image_detail_view, name='image_detail'),
+    path('like/', like_image_view, name='like_image'),
 ]
 
--- images/views.py
+++ images/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from decorators.decorators import ajax_required
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
 from .models import Image
@@ -25,8 +28,26 @@ def upload_image_view(request):
         form = ImageUploadForm(data=request.GET)
     return render(request, 'images/upload_image.html',
                   {'section': 'images', 'form':form})
-
+@login_required
 def image_detail_view(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request, 'images/image_detail.html', {'image':image, 'section':'images', })
-    
+
+#view for liking an image
+@ajax_required
+@login_required
+@require_POST
+def like_image_view(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users.add(request.user)
+            else:
+                image.users.remove(request.user)
+            return JsonResponse({'status':'ok'})
+        except:
+            pass
+    return JsonResponse({'status':'error'})
